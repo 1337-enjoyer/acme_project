cc7bc675763600725fc20ef30150dc62f50f4582
Anton Silaev <s1laev-aa@yandex.ru>
2026-02-17 16:17:36 +0300

    some updates

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,9 +1,23 @@
 # mypy: ignore-errors
-from django.views.generic import CreateView, DeleteView, ListView, UpdateView
+from django.views.generic import (
+    CreateView, DeleteView, DetailView, ListView, UpdateView
+)
 from django.urls import reverse_lazy
 
 from .forms import BirthdayForm
 from .models import Birthday
+from .utils import calculate_birthday_countdown
+
+
+class BirthdayDetailView(DetailView):
+    model = Birthday
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['birthday_countdown'] = calculate_birthday_countdown(
+            self.object.birthday
+        )
+        return context
 
 
 class BirthdayMixin:
@@ -13,7 +27,6 @@ class BirthdayMixin:
 
 class BirthdayFormMixin:
     form_class = BirthdayForm
-    template_name = 'birthday/birthday.html'
 
 
 class BirthdayCreateView(BirthdayMixin, BirthdayFormMixin, CreateView):
