045dfe5315214da85a505c4bb0d634838d6e9f46
Anton Silaev <s1laev-aa@yandex.ru>
2026-02-19 15:28:30 +0300

    madness finish

--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -7,6 +7,13 @@ from .validators import real_age
 User = get_user_model()
 
 
+class Tag(models.Model):
+    tag: models.CharField = models.CharField('Тег', max_length=20)
+
+    def __str__(self) -> str:
+        return self.tag
+
+
 class Birthday(models.Model):
     first_name: models.CharField = models.CharField('Имя', max_length=20)
     last_name: models.CharField = models.CharField(
@@ -17,6 +24,12 @@ class Birthday(models.Model):
     image = models.ImageField('Фото', upload_to='birthdays_images', blank=True)
     author: models.ForeignKey = models.ForeignKey(
         User, verbose_name='Автор записи', on_delete=models.CASCADE, null=True
+    )
+    tags: models.ManyToManyField = models.ManyToManyField(
+        Tag,
+        verbose_name='Теги',
+        blank=True,
+        help_text='Удерживайте Ctrl для выбора нескольких вариантов'
     )
 
     class Meta:
@@ -29,3 +42,17 @@ class Birthday(models.Model):
 
     def get_absolute_url(self):
         return reverse('birthday:detail', kwargs={'pk': self.pk})
+
+
+class Congratulation(models.Model):
+    text: models.TextField = models.TextField('Текст поздравления')
+    birthday: models.ForeignKey = models.ForeignKey(
+        Birthday,
+        on_delete=models.CASCADE,
+        related_name='congratulations',
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ('created_at',)
